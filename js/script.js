6ce501ef1f41c79f1c84b8d8299d5d32bcdb5046
Sean Wines <seanwinesdev@gmail.com>
2016-08-06 10:27:25 -0700

    Added menu toggle for Service's page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-$freeQuote = $('.free_quote');
+let $plastiDipContent = $('.plasti-dip-content'),
+    $vinylContent = $('.vinyl-content'),
+    $hydroContent = $('.hydro-content'),
+    $plasti = $('.plasti'),
+    $vinyl = $('.vinyl'),
+    $hydro = $('.hydro');
 
-// SHOW popUp
-$freeQuote.delay(2000).animate({bottom: '50px'}, 'slow');
+
+$(function () {
+
+// Toggle service menu item display
+  $('.service-items').on('click', 'li', function () {
+    $('li').removeClass('selected-service');
+    $(this).addClass('selected-service');
+
+    $pos = $(this).index();
+    if ($pos === 0) {
+      $plastiDipContent.fadeIn();
+      $plastiDipContent.siblings().hide();
+    } else if ($pos === 1) {
+      $vinylContent.fadeIn();
+      $vinylContent.siblings().hide();
+    } else {
+      $hydroContent.fadeIn();
+      $hydroContent.siblings().hide();
+    }
+  })
+
+  // SHOW popup
+  $('.free-quote').delay(2000).animate({bottom: '50px'}, 'slow');
+
+})
